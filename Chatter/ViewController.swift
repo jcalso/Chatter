3776a044b0e07bde92a6cdd0dd702c9803cf9954
John Calso <jcalso@umich.edu>
2020-01-23 17:23:14 -0500

    Initial Commit

---
+++ Chatter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatter
+//
+//  Created by Jonathan Calso on 1/23/20.
+//  Copyright © 2020 frankloni's pepperonis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
